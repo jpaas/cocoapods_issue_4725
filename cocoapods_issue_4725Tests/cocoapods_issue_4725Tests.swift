2d7d12fea867bce7c7f976f722d999c2083123d6
Julian Paas <julian.paas@osaaru.com>
2016-01-08 16:26:38 -0500

    initial commit

---
+++ cocoapods_issue_4725Tests/cocoapods_issue_4725Tests.swift
@@ -0,0 +1,36 @@
+//
+//  cocoapods_issue_4725Tests.swift
+//  cocoapods_issue_4725Tests
+//
+//  Created by Julian Paas on 2016-01-08.
+//  Copyright © 2016 Osaaru Corporation. All rights reserved.
+//
+
+import XCTest
+@testable import cocoapods_issue_4725
+
+class cocoapods_issue_4725Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
